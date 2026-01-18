58d66682ff3a98d5b1d9d1d15183467b1a1275dd
bindupatel-19 <bindugombe199@gmail.com>
2026-01-18 14:16:14 +0530

    add teams and contact us component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import Hero from "./components/Hero";
 import TrustedBy from "./components/TrustedBy";
 import Services from "./components/Services";
 import OurWork from "./components/OurWork";
-
+import Teams from "./components/Teams";
+import ContactUs from "./components/ContactUs";
+import { Toaster } from "react-hot-toast";
 const App = () => {
   const [theme, setTheme] = useState(
     localStorage.getItem("theme") ? localStorage.getItem("theme") : "light",
@@ -12,11 +14,14 @@ const App = () => {
 
   return (
     <div className="dark:bg-black relative">
+      <Toaster />
       <Navbar theme={theme} setTheme={setTheme} />
       <Hero />
       <TrustedBy />
       <Services />
       <OurWork />
+      <Teams />
+      <ContactUs />
     </div>
   );
 };
